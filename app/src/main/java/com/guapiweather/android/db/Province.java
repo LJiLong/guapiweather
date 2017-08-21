ee1fb40a6a841b2cdcbb021feced1cce1d6eec44
Venlo555 <476188961@qq.com>
2017-08-21 11:40:17 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/guapiweather/android/db/Province.java
@@ -0,0 +1,36 @@
+package com.guapiweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 林基隆 on 8/20 0020.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
